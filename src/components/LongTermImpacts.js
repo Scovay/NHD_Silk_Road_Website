b65d3e93721815399b288271e7a660167c33ff96
Jonathan Xu <scovay.xu@gmail.com>
2021-01-17 19:44:33 -0500

    Added 2 new webpages

---
+++ src/components/LongTermImpacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LongTermImpact = () => {
+  return (
+    <div className = "main-content">
+      <h2>People</h2>
+      <p></p>
+    </div>
+  )
+}
+
+export default LongTermImpact;
