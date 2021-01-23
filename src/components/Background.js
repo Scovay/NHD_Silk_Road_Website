1b1f04de057cedcc43fb9f198f781fc839f9cb6d
Jonathan Xu <scovay.xu@gmail.com>
2021-01-23 10:26:14 -0500

    Added More Content

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,8 +3,9 @@ import React from 'react';
 const About = () => (
   <div className="main-content">
     <h2>Background</h2>
-    <p>A bit about the Silk Road</p>
-    
+    <p>China during the Han dynasty sent out emissaries and led by Zhang Qian. He encountered different cultures and found superior horses from Greco-Bactrians descended from Alexander the Great’s army which he called the “Dayuan” which means “Great Ionians". When he found the horses, he reported to the current emperor Wu which then used them against the currently maundering Xiongnu which led the emperor to wonder what other goods and benefits they could get from trading with the west.</p>
+    <p>Silk Road, also called Silk Route was an ancient trade route, linking China with the West, that carried goods and ideas between the two great civilizations of Rome and China. Silk went westward, and wools, gold, and silver went east. China also received Nestorian Christianity and Buddhism (from India) via the Silk Road. The Silk Roads life span was from 130 BC to 220 AD</p>
+    <img src="https://www.chinadiscovery.com/assets/images/silk-road/maps/iron_silk_road-full.jpg" alt="Map of Silk Road" width="500" c></img>
   </div>
 );
 
