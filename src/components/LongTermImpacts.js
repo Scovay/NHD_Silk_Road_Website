bc947b3d3e3cd5a57dd499d3973ff15957d93192
Jonathan Xu <scovay.xu@gmail.com>
2021-01-23 11:32:53 -0500

    Added Impact Content

--- src/components/LongTermImpacts.js
+++ src/components/LongTermImpacts.js
@@ -3,8 +3,8 @@ import React from 'react';
 const LongTermImpact = () => {
   return (
     <div className = "main-content">
-      <h2>People</h2>
-      <p></p>
+      <h2>Long Term Impact</h2>
+      <p>Because of how long usage of the Silk was, 10, 25, or 50 years later cannot be described as very long. Instead, we look for periods like 100, 250, or 500 years after. First, religion spread. Buddhism went to the west while Christianity went to the east. Goods on both sides of the Silk Road became more available. The Chinese valued Roman metalworking and horses while the Romans valued Chinese silk and China (plates, bowls, etc.). In general, the economy grew a lot even multiple decades and centuries after the creation of the Silk Road.</p>
     </div>
   )
 }
--- src/components/ShortTermImpacts.js
+++ src/components/ShortTermImpacts.js
@@ -4,7 +4,7 @@ const ShortTermImpact = () => {
   return (
     <div className = "main-content">
       <h2>People</h2>
-      <p></p>
+      <p>A route was found to get from Rome to China while stopping in all the things in between. People weren’t necessarily extremely eager to use the Silk Road at first, but it grew into popularity shortly after. When early scouts discovered the great horses that the west had, the emperor used them against his enemies and was eager to find more treasures from the west.</p>
     </div>
   )
 }
