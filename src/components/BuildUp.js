0872202c8a262eecafe9052b362db50daccb2f85
Quellie <scovay.xu@gmail.com>
2021-01-29 11:39:36 -0500

    Added Zhang Qian Content

--- src/components/BuildUp.js
+++ src/components/BuildUp.js
@@ -4,7 +4,8 @@ const Items = () => {
   return (
     <div className = "main-content">
       <h2>Timeline</h2>
-      <h2>Zhang Qian acting as an emissionary</h2>
+      <h2>Zhang Qian and his role in starting the Silk Road</h2>
+      <p>Ambassador Zhangqian was a courageous explorer and a clever diplomatist. The Han Dynasty fought with the Huns in northwestern China. Emperor Wu sent Zhangqian to seek help westward, specifically in Darouzhi, but he was arrested by the Huns. 10 years later, Zhangqian escaped the Huns and took his trip to Dayuan and Kangju, or modern day Uzbekistan and Balkh which is modern Afghanistan. After many trips, Zhangqian finally arrived in Darouzhi, just to find that the emperor refused the allying request. Zhangqian reported back to Chang’an without help, but he did acquire information about western regions (which would help him later establish the Silk Road). He would later get more help to defeat the Huns. In the end, his two trips broke down the barriers that separated the west from the east. This built the great foundation for the communication between China, Central Asia, and the Roman Empire.</p>
       <img src = "https://www.chinadiscovery.com/assets/images/silk-road/history/zhangqian-650.jpg" alt = "Zhang Qian Meeting With the West"></img>
     </div>
   )
