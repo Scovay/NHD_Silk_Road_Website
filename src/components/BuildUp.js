f0b9adf6fe4f73cefec704f706d2065dcdaff7d6
Quellie <scovay.xu@gmail.com>
2021-01-29 12:15:58 -0500

    Added Major Events To Build Up

--- src/components/BuildUp.js
+++ src/components/BuildUp.js
@@ -4,9 +4,37 @@ const Items = () => {
   return (
     <div className = "main-content">
       <h2>Timeline</h2>
-      <h2>Zhang Qian and his role in starting the Silk Road</h2>
-      <p>Ambassador Zhangqian was a courageous explorer and a clever diplomatist. The Han Dynasty fought with the Huns in northwestern China. Emperor Wu sent Zhangqian to seek help westward, specifically in Darouzhi, but he was arrested by the Huns. 10 years later, Zhangqian escaped the Huns and took his trip to Dayuan and Kangju, or modern day Uzbekistan and Balkh which is modern Afghanistan. After many trips, Zhangqian finally arrived in Darouzhi, just to find that the emperor refused the allying request. Zhangqian reported back to Chang’an without help, but he did acquire information about western regions (which would help him later establish the Silk Road). He would later get more help to defeat the Huns. In the end, his two trips broke down the barriers that separated the west from the east. This built the great foundation for the communication between China, Central Asia, and the Roman Empire.</p>
+      <hr></hr>
+      <h1>Major World Events</h1><br></br><br></br>
+      <h2>Alexander the Great</h2>
+      <p className = "text-blurb">
+      Alexander the Great made a ton of development to the making of the Silk Road. At about 330 BC, he successfully defeated Greece, Egypt, the Persian Empire, and Northern India. During his tremendous conquest, he founded many important trading cities which later became financial centers of trade. At around 200 BC, Alexander finally reached Kashgar which was the very very first contact between China and Europe.
+      </p>
+      <hr></hr>
+      <h2>Banchao Conquered the Western Regions</h2>
+      <p className = "text-blurb">
+      After the fall of the Western Han Dynasty, the Silk Road had shut down because of frequent wars in the Western Region between the countries in that region. In the Eastern Han Dynasty, Banchao was an envoy to strengthen communication between Eastern Han and the Western Region. During his 31 years in the Western Regions, Banchao had taken control of more than 50 small countries, and was able to open the Silk Road again.
+      </p>
+      <hr></hr>
+      <h2>Tang Dynasty Reopens the Route</h2>
+      <p className = "text-blurb">
+      The prosperity of the Silk Road reached its height in the Tang Dynasty (618~907AD). The Tang Dynasty was the most powerful and wealthy nation in the world. Its conquest over the West and central Asia ensured the trade along the Silk Road. The emperors of Tang carried out friendly diplomacy policy, and welcomed foreign envoys, merchants and travelers, which made Chang'an a cosmopolitan. In the Tang Dynasty, the Maritime Silk Route also were pioneered by Chinese. The envoys sailed through the Indian Ocean to Persian Gulf and Red Sea, to explore Persia, Egypt, Aksum and Somalia.
+      </p>
+      <hr></hr>
+      <h2>Mongol Age</h2>
+      <p className = "text-blurb">
+      During the 13th century, the Mongols did a great expansion into Asia, which brought a hundred years’ stability to the Silk Road. Merchandise circulated well from China, via Central Asia, to Europe. The Mongol sent a diplomat Rabban Bar Sauma who visited the courts of Europe in 1287~1288 and returned back to China with a detailed report about Europe. At the same time, the world famous traveler Marco Polo traveled the Silk Road to China, and met by the Mongol emperor. His tales, the Travels of Marco Polo, were fully read by Westerns, which helped Europeans learn much about East and China.
+      </p>
+      <hr></hr>
+      <h2>Roman Empire</h2>
+      <p className = "text-blurb">
+      In the first century in BC, the Roman Empire conquered Seleucid Empire and Egypt Empire. Through central Asia, intercontinental trade and communication became regular, and blossomed on an unprecedented scale. The Roman Empire built two ports in Barygaza and Barbarricum to trade with the Central Asian Silk Road. They traded spices, perfumes, and exchanged silk, porcelain, jades from China. The Romans were fancy about Chinese silk very much which became luxurious clothing materials for women. Both Roman and China attached much importance to the international trade on the Silk Road. The Silk Road was under protection of Roman and Chinese armies.
+      </p>
+      <hr></hr>
+      <h2>Zhangqian’s visit to the western regions</h2>
+      <p className = "text-blurb">Ambassador Zhangqian was a courageous explorer and a clever diplomatist. The Han Dynasty fought with the Huns in northwestern China. Emperor Wu sent Zhangqian to seek help westward, specifically in Darouzhi, but he was arrested by the Huns. 10 years later, Zhangqian escaped the Huns and took his trip to Dayuan and Kangju, or modern day Uzbekistan and Balkh which is modern Afghanistan. After many trips, Zhangqian finally arrived in Darouzhi, just to find that the emperor refused the allying request. Zhangqian reported back to Chang’an without help, but he did acquire information about western regions (which would help him later establish the Silk Road). He would later get more help to defeat the Huns. In the end, his two trips broke down the barriers that separated the west from the east. This built the great foundation for the communication between China, Central Asia, and the Roman Empire.</p>
       <img src = "https://www.chinadiscovery.com/assets/images/silk-road/history/zhangqian-650.jpg" alt = "Zhang Qian Meeting With the West"></img>
+      <hr></hr>
     </div>
   )
 }
